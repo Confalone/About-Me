92398162767246e57108f15bf6ff4564b243a9c9
Tyler <Tconfalone@gmail.com>
2018-01-13 18:26:40 -0800

    lab3 struggling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var userPoints = 0;
 
@@ -17,28 +17,28 @@ console.log(answer);
 //check if user answer is correct
 //What is correct?
 //find out what the ansewr is?
-//to lowercase 
+//to lowercase
 //use confirm box
 
 if (answer=== 'yes' || answer === 'y'){
-    //if it's correct give tehm a point
-    userPoints++;
-    console.log(userPoints);
+  //if it's correct give tehm a point
+  userPoints++;
+  console.log(userPoints);
 } else {
-    alert('I love my couch sometimes too.')
+  alert('I love my couch sometimes too.');
 }
 
-alert ('You have ' + userPoints + ' point.')
+alert ('You have ' + userPoints + ' point.');
 
 var answerSwim = prompt('Do you like to go swimming?' , 'type yes or no').toLowerCase();
 
 console.log(answerSwim);
 
 if (answerSwim === 'yes' || answer === 'y'){
-    userPoints++;
-    console.log(userPoints);
+  userPoints++;
+  console.log(userPoints);
 } else {
-    alert('That\'s cool, someteims it\'s just too cold for getting wet.');
+  alert('That\'s cool, someteims it\'s just too cold for getting wet.');
 }
 
 alert ('You now have a score of' + userPoints);
@@ -48,10 +48,10 @@ var answerBike = prompt('Do you like to ride bicycles? ' , 'type yes or no').toL
 console.log(answerBike);
 
 if (answerBike === 'yes' || answerBike === 'y'){
-    userPoints++;
-    console.log(userPoints);
+  userPoints++;
+  console.log(userPoints);
 } else {
-    alert('It\'s prob safer to ride the bus anyway.');
+  alert('It\'s prob safer to ride the bus anyway.');
 }
 alert('You are running a score of ' + userPoints);
 
@@ -60,24 +60,79 @@ var answerComputers = prompt ('Do you like computers?' , 'type yes or no').toLow
 console.log(answerComputers);
 
 if (answerComputers === 'yes' || answerComputers === 'y'){
-    userPoints++;
-    console.log(userPoints);
+  userPoints++;
+  console.log(userPoints);
 } else {
-    alert('Wow I am schocked.');
+  alert('Wow I am schocked.');
 }
 
 var answerCoding = prompt ('I bet you like coding right?' , 'type yes or no').toLowerCase();
 
 if (answerCoding === 'yes' || answerCoding === 'y'){
-    userPoints++;
-    console.log(userPoints);
+  userPoints++;
+  console.log(userPoints);
 } else {
-    alert('Again, shocked');
+  alert('Again, shocked');
 }
-if (userPoints > 3){
-    alert('Your final score is ' + userPoints + 'We have a good amount in common.');
-} else {
-    alert('Your fimnal score is ' + userPoints + 'We don\'t have too much in common, but i still love you and hope you have an amazing day');
-}
-alert('Your final score is ' + userPoints + '');
 
+//question 6
+//what is my fav number?
+//too high too low
+//4 guesses
+
+for (var i =4; i >= 0; i--){
+  var answerFavnumber = parseInt (prompt(answerFavnumber));
+  console.log(answerFavnumber);
+  if (answerFavnumber > 9){
+    alert('Too high ' + ' guesses left.')}
+  if (answerFavnumber < 9){
+    alert ('Too low ' + ' guesses left.')}
+  else {
+    alert ('Bingo.');
+    userPoints++;
+    break;
+  }
+//ar answerFavnumber = prompt ('What is my favorite number, pick between 1-10?');
+//while (answerFavnumber != 9) {
+//alert('Nope, ' + answerFavnumber + ' too high.');
+//var answerFavnumber = prompt ('What is my favorite number, pick between 1-10?');
+//}
+//if (answerFavnumber > 9) {
+//alert('Nope, ' + answerFavnumber + ' too high.');
+//var answerFavnumber = prompt ('What is my favorite number, pick between 1-10?');
+//}
+//if (answerFavnumber < 9); {
+//alert('Nope, ' + answerFavnumber + ' too low,')
+//var answerFavnumber = prompt ('What is my favorite number, pick between 1-10?');
+//}
+//var answerFavnumber = prompt ('What is my favorite number, pick between 1-10?');
+
+//for (var i = 4; i >= 0; i--); {
+  //var answerFavnumber = parseInt(prompt('What is my favorite number, pick between 1-10?'));
+  //console.log(answerFavnumber);
+  //if (answerFavnumber > 9) {
+    //alert('Nope, ' + user + ' too high');
+  //}
+  //if (answerFavnumber < 9) {
+    //alert('Nope, ' + user + ' too low');
+ // }
+  //else (answerFavnumber === 9); {
+    //alert('You\'re right ' + user);
+    //userPoints++;
+    //break;
+  //}
+//}
+
+//question 7
+//fav sports
+//6 attempts to get 1 answer correct
+//as tries are used up;  answer displys a mesage to the user indicatingall the pos answers
+//var sports = prompt ('What is my favorite sport.');
+
+
+if (userPoints > 3){
+  alert('Your final score is ' + userPoints + '  We have a good amount in common.');
+} else {
+  alert('Your final score is ' + userPoints + '  We don\'t have too much in common, but i still love you and hope you have an amazing day');
+}{}
+alert('Your final score is ' + userPoints);
